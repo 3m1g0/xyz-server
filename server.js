828fc693f7b32e4688e637c760f15e1f7935fb51
KT <ktdaiict@gmail.com>
2017-07-28 16:15:39 +0530

    changed mongodb url

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require('express'),
   app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://heroku_hjlz1cm3:Teja%40123@ds127443.mlab.com:27443/heroku_hjlz1cm3');
+mongoose.connect('mongodb://xyzserver:xyzserver@ds127443.mlab.com:27443/heroku_hjlz1cm3');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
